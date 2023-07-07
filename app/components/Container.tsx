543682f0efe60b0e71c4faa7897cb0d55d6840bb
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-07 20:54:40 -0300

    First commit, we configure and create the navbar component

---
+++ app/components/Container.tsx
@@ -0,0 +1,16 @@
+'use cliente' //There is the differential the client components and server component for I don't have problems
+
+// type Props = {}
+interface Props {
+    children: React.ReactNode
+}
+
+const Container: React.FC<Props> = ({ children }) => {
+    return (
+        <div className="amx-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+            {children}
+        </div>
+    )
+}
+
+export default Container
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+type Props = {}
+
+const Search = (props: Props) => {
+    return (
+        <div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md trasition cursor-pointer'>
+            <div className="flex flex-row items-center justify-between">
+                <div className="text-sm font-semibold px-6">
+                    Anywhere
+                </div>
+                <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+                    Any Week
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search

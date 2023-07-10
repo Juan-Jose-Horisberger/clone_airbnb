96d54b64cc7e86ab9121c65aaeefdd8aeecd74c3
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-09 21:35:07 -0300

    new state global for the model of login

---
+++ app/hooks/useLoginModal.ts
@@ -0,0 +1,16 @@
+//model of Login
+import { create } from 'zustand'
+
+interface LoginModalStore {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+}
+
+const useLoginModal = create<LoginModalStore>((set) => ({ //state global
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}))
+
+export default useLoginModal

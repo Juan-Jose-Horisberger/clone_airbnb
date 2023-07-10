66e996337d0b42c0fcea999f8a33d3ff33222283
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-09 21:28:08 -0300

    We create our first route of register

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,14 +1,26 @@
-// import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
+import prisma from "@/app/libs/prismadb"
+import { NextResponse } from "next/server"
 
-// export async function POST(
-//     request: Request
-// ) {
-//     const body = await request.json()
-//     const {
-//         email,
-//         name,
-//         password
-//     } = body
+export async function POST(
+    request: Request
+) {
+    const body = await request.json() //We receive the information of the user who wants to register
+    const {
+        email,
+        name,
+        password
+    } = body
 
-//     const hashedPassword = await bcrypt(password, 12)
-// }
+    const hashedPassword = await bcrypt.hash(password, 12) //hashed password
+
+    const user = await prisma.user.create({ //We create the new user
+        data: {
+            email,
+            name,
+            hashedPassword
+        }
+    })
+
+    return NextResponse.json(user)
+}

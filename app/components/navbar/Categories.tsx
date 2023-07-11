885c8449b1e9bf51a515f3baaa1ee07604724771
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-11 15:57:01 -0300

    add new categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import Container from "../Container"
+import { IoDiamond } from "react-icons/io5"
+import { BsSnow } from 'react-icons/bs'
 import { FaSkiing } from "react-icons/fa"
-import { TbBeach } from "react-icons/tb"
-import { GiWindmill } from "react-icons/gi"
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb"
+import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from "react-icons/gi"
 import { MdOutlineVilla } from "react-icons/md"
 
 import CategoryBox from "../CategoryBox"
@@ -27,8 +29,66 @@ export const categories = [
         icon: MdOutlineVilla,
         description: 'This property is modern!'
     },
-
-
+    {
+        label: 'Countryside',
+        icon: TbMountain,
+        description: 'This property is in the countryside!'
+    },
+    {
+        label: 'Pools',
+        icon: TbPool,
+        description: 'This property has a pool!'
+    },
+    {
+        label: 'Islands',
+        icon: GiIsland,
+        description: 'This property is on an island!'
+    },
+    {
+        label: 'Lake',
+        icon: GiBoatFishing,
+        description: 'This property is close to a lake!'
+    },
+    {
+        label: 'Skiing',
+        icon: FaSkiing,
+        description: 'This property has skiing activities!'
+    },
+    {
+        label: 'Castles',
+        icon: GiCastle,
+        description: 'This property is in a castle!'
+    },
+    {
+        label: 'Camping',
+        icon: GiForestCamp,
+        description: 'This property has camping activities!'
+    },
+    {
+        label: 'Artic',
+        icon: BsSnow,
+        description: 'This property has camping activities!'
+    },
+    {
+        label: 'Cave',
+        icon: GiCaveEntrance,
+        description: 'This property is in a cave!'
+    },
+    {
+        label: 'Desert',
+        icon: GiCactus,
+        description: 'This property is in the desert!'
+    },
+    {
+        label: 'Barns',
+        icon: GiBarn,
+        description: 'This property is in the barn!'
+    },
+    {
+        label: 'Lux',
+        icon: IoDiamond,
+        description: 'This property is in the barn!'
+    },
 ]
 
 const Categories = (props: Props) => {

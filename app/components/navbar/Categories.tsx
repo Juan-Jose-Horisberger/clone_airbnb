4ac20b918e29ae2c29ddfc216ea17ccc45073859
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-10 21:09:48 -0300

    we create new component Categories

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Container from "../Container"
+
+type Props = {}
+
+const Categories = (props: Props) => {
+    return (
+        <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-auto">
+
+            </div>
+        </Container>
+    )
+}
+
+export default Categories

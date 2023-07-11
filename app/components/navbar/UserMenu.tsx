30580a12914a84974ea12f9b71dcbcff7edab222
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-11 16:26:50 -0300

    We change styles in UserMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -29,7 +29,7 @@ const UserMenu: React.FC<Props> = ({ currentUser }) => {
                     className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                     Airbnb your home
                 </div>
-                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full">
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer">
                     <AiOutlineMenu />
                     <div className='hidden md:block'>
                         <Avatar src={currentUser?.image} />

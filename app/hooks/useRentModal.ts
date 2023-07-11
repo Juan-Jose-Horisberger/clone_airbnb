07a839561b727b642d1b5759ef5badebc1dc9295
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-11 18:53:21 -0300

    we create a global state new and new component

---
+++ app/hooks/useRentModal.ts
@@ -0,0 +1,16 @@
+//model of Login
+import { create } from 'zustand'
+
+interface RentModalStore {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+}
+
+const useLoginModal = create<RentModalStore>((set) => ({ //state global
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}))
+
+export default useLoginModal

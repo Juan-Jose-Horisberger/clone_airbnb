f86da1245a4dbdef97b324c3f6484066c98201d3
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-12 20:23:01 -0300

    we solution errors to the import Map

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,7 +9,7 @@ import { type Category as CategoryType } from "@/app/types"
 import CategoryInput from '../inputs/CategoryInput'
 import { FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../inputs/CountrySelect'
-import Map from '../Map'
+import dynamic from 'next/dynamic' //module importer dynamically
 
 enum STEPS {
     CATEGORY = 0,
@@ -50,6 +50,10 @@ const RentModal = () => {
 
     const category = watch('category')
     const location = watch('location')
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location])
 
 
     const setCustomValue = (id: string, value: any): void => {
@@ -108,7 +112,9 @@ const RentModal = () => {
                     value={location}
                     onChange={(value) => setCustomValue('location', value)}
                 />
-                <Map />
+                <Map
+                    center={location?.latlng}
+                />
             </div>
         )
     }

72495e5d2a0cdac41f8a02c376d48db8aad0ffb5
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-12 19:54:52 -0300

    we configure the dependency leaflet and we add styles globals

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,10 +1,38 @@
 'use client'
 
-type Props = {}
+import L from "leaflet"
+import { MapContainer, TileLayer, Marker } from "react-leaflet"
 
-const Map = (props: Props) => {
+import "leaflet/dist/leaflet.css"
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import markerShadow from "leaflet/dist/images/marker-shadow.png"
+
+//leaflet package is no exactly supported in react it's an open source project. we need to write the following:
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src
+})
+
+interface Props {
+    center?: number[]
+}
+
+const Map: React.FC<Props> = ({ center }) => {
     return (
-        <div>Map</div>
+        <MapContainer
+            center={center as L.LatLngExpression || [51, -0.09]}
+            zoom={center ? 4 : 2}
+            scrollWheelZoom={false}
+            className="h-[35vh] rounded-lg"
+        >
+
+        </MapContainer>
     )
 }
 

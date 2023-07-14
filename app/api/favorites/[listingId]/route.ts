bd5161350052a89c45335b06037dc81e71cbfccd
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-14 19:36:51 -0300

    we create new function for delete favorites

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -7,7 +7,7 @@ interface IParams {
     listingId?: string
 }
 
-export default async function POST(
+export default async function POST( //Add favorite
     request: Request,
     { params }: { params: IParams }
 ) {
@@ -25,6 +25,37 @@ export default async function POST(
 
     let favoriteIds = [...(currentUser.favoriteIds || [])]
     favoriteIds.push(listingId)
+
+    const user = await prisma.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data: {
+            favoriteIds
+        }
+    })
+
+    return NextResponse.json(user)
+}
+
+export async function DELETE( //Delete favorite
+    request: Request,
+    { params }: { params: IParams }
+) {
+    const currentUser = await getCurrentUser()
+
+    if (!currentUser) {
+        return NextResponse.error()
+    }
+
+    const { listingId } = params
+
+    if (!listingId || typeof listingId !== 'string') {
+        throw new Error('Invalid ID')
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])]
+    favoriteIds = favoriteIds.filter((id) => id !== listingId)
 
     const user = await prisma.user.update({
         where: {

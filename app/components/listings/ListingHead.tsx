b3d221787fb69e15fa64002819029d59d7b00988
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-18 14:29:09 -0300

    new component ListingHead in this, we will show the information of the listing

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ListingHead = (props: Props) => {
+    return (
+        <div>ListingHead</div>
+    )
+}
+
+export default ListingHead
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,3 +1,6 @@
+import Container from '@/app/components/Container';
+import ListingHead from '@/app/components/listings/ListingHead';
+import { categories } from '@/app/constantes';
 import { type SafeUser, type SafeListing } from '@/app/types'
 import { Reservation } from '@prisma/client'
 import { useMemo } from 'react'
@@ -15,11 +18,25 @@ const ListingClient: React.FC<Props> = ({
     currentUser
 }) => {
     const category = useMemo(() => {
-
-    }, [])
+        return categories.find((item) =>
+            item.label === listing.category
+        )
+    }, [listing.category])
 
     return (
-        <div>ListingClient</div>
+        <Container>
+            <div className='max-w-screen-lg mx-auto'>
+                <div className='flex flex-col gap-6'>
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </Container>
     )
 }
 

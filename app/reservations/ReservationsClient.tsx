9132b6f6524b23b968b2c63e2151050265151531
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-21 20:30:30 -0300

    we reuse components heading, lintingcard, heading

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,5 +1,14 @@
 'use client'
 
+import { Toast } from "react-hot-toast"
+import axios from "axios"
+import { useCallback, useState } from "react"
+import { useRouter } from "next/navigation"
+
+import Heading from "../components/Heading"
+import Container from "../components/Container"
+import ListingCard from "../components/listings/ListingCard"
+
 import { type SafeReservations, type SafeUser } from "../types"
 
 interface Props {
@@ -7,9 +16,17 @@ interface Props {
     currentUser?: SafeUser | null
 }
 
-const ReservationsClient = (props: Props) => {
+const ReservationsClient: React.FC<Props> = ({
+    reservations,
+    currentUser
+}) => {
     return (
-        <div>ReservationsClient</div>
+        <Container>
+            <Heading
+                title="Reservations"
+                subtitle="Bookings on your properties"
+            />
+        </Container>
     )
 }
 

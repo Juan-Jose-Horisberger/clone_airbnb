226f5216aa0bacc8af930a697d4df538b8ff9896
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-24 18:37:00 -0300

    we create new state global in hooks (useSearchModal)

---
+++ app/hooks/useSearchModal.ts
@@ -0,0 +1,16 @@
+//model of Search
+import { create } from 'zustand'
+
+interface SearchModalStore {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+}
+
+const useSearchModal = create<SearchModalStore>((set) => ({ //state global
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}))
+
+export default useSearchModal

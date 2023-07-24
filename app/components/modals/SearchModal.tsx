2f0825d8f79b4bdcd3633e632e001a5c4b21a8c9
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-24 19:47:22 -0300

    functionalities onSubmit, onNext, onBack

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,12 +1,102 @@
 'use client'
 
-import useSearchModal from "@/app/hooks/useSearchModal"
+import { formatISO } from "date-fns"
+import qs from "query-string"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useCallback, useMemo, useState } from "react"
+import { Range } from "react-date-range"
+
 import Modal from "./Modal"
+import { CountrySelectValue } from "../inputs/CountrySelect"
+
+import useSearchModal from "@/app/hooks/useSearchModal"
+import dynamic from "next/dynamic"
+
+enum STEPS {
+    LOCATION = 0,
+    DATE = 1,
+    INFO = 2
+}
 
 type Props = {}
 
 const SearchModal = (props: Props) => {
+    const router = useRouter()
+    const params = useSearchParams()
     const searchModal = useSearchModal()
+
+    const [location, setLocation] = useState<CountrySelectValue>()
+    const [step, setStep] = useState(STEPS.LOCATION)
+    const [guestCount, setGuestCount] = useState(1)
+    const [roomCount, setRoomCount] = useState(1)
+    const [bathroomCount, setBathroomCount] = useState(1)
+    const [dateRange, setDateRange] = useState<Range>({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+    })
+
+    const map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location])
+
+    const onBack = useCallback(() => {
+        setStep((value) => value - 1)
+    }, [])
+
+    const onNext = useCallback(() => {
+        setStep((value) => value + 1)
+    }, [])
+
+    const onSubmit = useCallback(async () => {
+        if (step !== STEPS.INFO) {
+            return onNext()
+        }
+
+        let currentQuery = {}
+
+
+        if (params) {
+            currentQuery = qs.parse(params.toString())
+        }
+
+        const updatedQuery: any = {
+            ...currentQuery,
+            locationValue: location?.value,
+            guestCount,
+            roomCount,
+            bathroomCount
+        }
+
+        if (dateRange.startDate) {
+            updatedQuery.startDate = formatISO(dateRange.startDate) //string
+        }
+
+        if (dateRange.endDate) {
+            updatedQuery.endDate = formatISO(dateRange.endDate)
+        }
+
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, { skipNull: true })
+
+        setStep(STEPS.LOCATION)
+        searchModal.onClose()
+
+        router.push(url)
+    }, [
+        step,
+        searchModal,
+        location,
+        router,
+        guestCount,
+        roomCount,
+        bathroomCount,
+        dateRange,
+        onNext,
+        params
+    ])
 
     return (
         <Modal
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import RentModal from './components/modals/RentModal'
 
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
+import SearchModal from './components/modals/SearchModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,9 +34,10 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
-          <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className='pb-20 pt-28'>

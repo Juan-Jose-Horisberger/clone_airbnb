115961a7081a57a239e3a8efb0dea62bc064fb03
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-26 19:08:33 -0300

    page.tsx we add force-dynamic to solve an error in production

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import Container from "@/app/components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";

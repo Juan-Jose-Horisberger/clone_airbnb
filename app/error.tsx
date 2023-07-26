920664456935249f05bb27af267985bdf6b7b0d6
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-26 16:14:45 -0300

    we create component for drive errors

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect } from "react"
+import EmptyState from "./components/EmptyState"
+
+interface Props {
+    error: Error
+}
+
+const ErrorState: React.FC<Props> = ({
+    error
+}) => {
+
+    useEffect(() => {
+        console.log(error)
+    }, [error])
+
+    return (
+        <EmptyState
+            title="Uh Oh"
+            subtitle="Something went wrong"
+        />
+    )
+}
+
+export default ErrorState

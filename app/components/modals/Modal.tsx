2653ba9ee9c0c877f62ae2bec4facffdbc58b159
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2023-07-31 17:49:57 -0300

    modal style changes

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -66,7 +66,7 @@ const Modal: React.FC<Props> = ({
 
     return (
         <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none bg-neutral-800/70'>
-            <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mt-20 mx-auto h-full lg:h-auto md:h-auto'>
+            <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 md:mt-20 mx-auto h-full lg:h-auto md:h-auto'>
                 {/* CONTENT */}
                 <div className={`
                     translate
@@ -75,7 +75,7 @@ const Modal: React.FC<Props> = ({
                     ${showModal ? 'translate-y-0' : 'translate-y-full'}
                     ${showModal ? 'opacity-100' : 'opacity-0'}
                 `}>
-                    <div className='translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                    <div className='translate h-auto lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
                         {/* HEADER */}
                         <div className='flex items-center p-6 rounded-t justify-center relative border-b-[1px]'>
                             <button onClick={handleClose} className='p-1 border-0 hover:opacity-70 transition absolute left-9'>
